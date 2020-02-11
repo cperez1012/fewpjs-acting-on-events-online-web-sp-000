7b97683657d16b1835e81d6932fab1bd8ff744a9
Christian Perez <perezchristian1012@gmail.com>
2020-02-11 01:34:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
 
 function moveDodgerRight() {
